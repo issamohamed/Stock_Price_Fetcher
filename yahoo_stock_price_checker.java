f5517698915e66b7114784e72300f8530416a68d
Issa Mohamed <48192737+issamohamed@users.noreply.github.com>
2023-07-03 18:47:15 -0500

    Update yahoo_stock_price_checker.java
    resolving class issues trial 1

--- yahoo_stock_price_checker.java
+++ yahoo_stock_price_checker.java
@@ -1,37 +1,43 @@
-private static final String API_URL = "https://finance.yahoo.com/quote/";
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
-public static void main(String[] args) {
+public class StockPriceFetcher {
+    private static final String API_URL = "https://finance.yahoo.com/quote/";
 
-    String stockSymbol = "AAPL"; // example: Apple Inc.
+    public static void main(String[] args) {
+        String stockSymbol = "AAPL"; // example: Apple Inc.
 
-    try {
-        // Creating the URL object with the desired stock symbol
-        URL url = new URL(API_URL + stockSymbol);
+        try {
+            // Creating the URL object with the desired stock symbol
+            URL url = new URL(API_URL + stockSymbol);
 
-        // Opening the connection and setting the request method
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestMethod("GET");
+            // Opening the connection and setting the request method
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
 
-        // Reading the response from the API
-        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        StringBuilder response = new StringBuilder();
-        String line;
-        while ((line = reader.readLine()) != null) {
-            response.append(line);
-        }
-
-        // Parsing the response and extracting the current stock price
-        String[] lines = response.toString().split("\n");
-        for (String l : lines) {
-            if (l.contains("regularMarketPrice")) {
-                String[] split = l.split(":");
-                String price = split[1].trim();
-                price = price.substring(0, price.length() - 2);
-                System.out.println("The current price for " + stockSymbol + " is: $" + price);
+            // Reading the response from the API
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            StringBuilder response = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
             }
-        }
 
-    } catch (Exception e) {
-        e.printStackTrace();
+            // Parsing the response and extracting the current stock price
+            String[] lines = response.toString().split("\n");
+            for (String l : lines) {
+                if (l.contains("regularMarketPrice")) {
+                    String[] split = l.split(":");
+                    String price = split[1].trim();
+                    price = price.substring(0, price.length() - 2);
+                    System.out.println("The current price for " + stockSymbol + " is: $" + price);
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
