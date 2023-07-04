d36015af89cf9e926e11727d7ef5402c1c5c1a65
Issa Mohamed <48192737+issamohamed@users.noreply.github.com>
2023-07-03 20:03:14 -0500

    Update yahoo_stock_price_checker.java
    added error statement and prompts to select stock of choice

--- yahoo_stock_price_checker.java
+++ yahoo_stock_price_checker.java
@@ -1,27 +1,53 @@
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-
-import org.json.JSONException;
-import org.json.JSONObject;
+import java.util.Scanner;
 
 public class StockPriceFetcher {
     private static final String API_KEY = "99LZY9F61GXU1LAF";
-    private static final String API_URL = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=";
 
     public static void main(String[] args) {
-        String stockSymbol = "AAPL"; // Example: Apple Inc.
+        Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            System.out.print("What stock would you like to look at today? (or 'exit' to quit) ");
+            String input = scanner.nextLine();
+
+            if (input.equalsIgnoreCase("exit")) {
+                break;
+            }
+
+            String stockSymbol = input.toUpperCase();
+
+            if (fetchAndDisplayStockPrice(stockSymbol)) {
+                String choice;
+                do {
+                    System.out.print("Would you like to look at another stock? (yes/no) ");
+                    choice = scanner.nextLine();
+                    if (!choice.equalsIgnoreCase("yes") && !choice.equalsIgnoreCase("no")) {
+                        System.out.println("Please type in 'yes' or 'no'!");
+                    }
+                } while (!choice.equalsIgnoreCase("yes") && !choice.equalsIgnoreCase("no"));
+
+                if (choice.equalsIgnoreCase("no")) {
+                    break;
+                }
+            } else {
+                System.out.println("Please insert a valid stock!");
+            }
+        }
+    }
+
+    private static boolean fetchAndDisplayStockPrice(String symbol) {
         try {
-            // Creating the URL object with the desired stock symbol and API key
-            URL url = new URL(API_URL + stockSymbol + "&apikey=" + API_KEY);
+            URL url = new URL("https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" + symbol + "&apikey=" + API_KEY);
 
-            // Opening the connection and setting the request method
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
 
-            // Reading the response from the API
             BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
             StringBuilder response = new StringBuilder();
             String line;
@@ -29,24 +55,17 @@ public class StockPriceFetcher {
                 response.append(line);
             }
 
-            // Parsing the response and extracting the current stock price
-            String price = parseStockPrice(response.toString());
-            System.out.println("The current price for " + stockSymbol + " is: $" + price);
+            JSONObject data = new JSONObject(response.toString());
+            JSONObject globalQuote = data.getJSONObject("Global Quote");
+
+            String fetchedSymbol = globalQuote.getString("01. symbol");
+            String price = globalQuote.getString("05. price");
+
+            System.out.println("The current price for " + fetchedSymbol + " is: $" + price);
+            return true;
 
         } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static String parseStockPrice(String json) {
-        try {
-            JSONObject data = new JSONObject(json);
-            JSONObject globalQuote = data.getJSONObject("Global Quote");
-            String price = globalQuote.getString("05. price");
-            return price;
-        } catch (JSONException e) {
-            e.printStackTrace();
-            return null;
+            return false;
         }
     }
 }
